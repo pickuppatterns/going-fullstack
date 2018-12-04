5e2dc1744346d1140f85304dc990f79158a71240
‘Michael <pickup.patterns@gmail.com>
2018-12-04 15:11:17 -0800

    setup and seeing json data in postman and on the server clt

--- server/server.js
+++ server/server.js
@@ -4,8 +4,20 @@ console.log('i am the server file');
 const express = require('express');
 const app = express();
 
+const fs = require('fs');
 
+function readData() {
+  // we don't normally use sync, but fine for today
+  const data = fs.readFileSync('./data/collection.json', 'utf8');
+  return JSON.parse(data);
+}
+//middleware body parser
+app.use(express.json());
 
+app.get('/api/collection', (req, res) => {
+  const albums = readData();
+  res.json(albums);
+});
 const PORT = 3000;
 app.listen(PORT, () => 
 {
