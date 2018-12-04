109855214faa04459b8ebf2ea86c16378457bf7d
‘Michael <pickup.patterns@gmail.com>
2018-12-04 15:46:10 -0800

    added save functionality to server js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const shortid = require('shortid');
 
 console.log('i am the server file');
  
 
 
 function readData() {
-  // we don't normally use sync, but fine for today
+  // don't make this a habbit
   const data = fs.readFileSync('./data/albums.json', 'utf8');
   return JSON.parse(data);
 }
+function saveData(albums) {
+  const json = JSON.stringify(albums, true, 2);
+  fs.writeFileSync('./data/albums.json', json);
+}
+
 //middleware body parser
 app.use(express.json());
 
@@ -18,6 +24,16 @@ app.get('/api/albums', (req, res) => {
   const albums = readData();
   res.json(albums);
 });
+app.post('api/albums', (req, res) =>{
+
+  const albums = readData();
+  const album = req.body;
+  albums.id = shortid.generate();
+  albums.push(album);
+  saveData(albums);
+  res.json(album);
+});
+
 const PORT = 3000;
 app.listen(PORT, () => 
 {
