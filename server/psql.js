4d789001ad92cb6bcfcbc20827946f06e961b594
‘Michael <pickup.patterns@gmail.com>
2018-12-05 13:00:44 -0800

    updated psl logic

--- server/psql.js
+++ server/psql.js
@@ -1,23 +1,25 @@
+
+const fs = require('fs');
 const pg = require ('pg');
 const Client = pg.Client;
 const databaseUrl = 'postgres://localhost:5432/albums';
 const client = new Client(databaseUrl);
 
-client.connect()
-.then(() => {
-    return client.query(`
-    CREATE TABLE IF NOT EXISTS albums (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(256) NOT NULL,
-        track VARCHAR(256),
-        start_date DATE
-    )`)
-});
+client.connect();
 
 client.query(`
-SELECT *FROM albums;
+SELECT name, year, description, rating FROM albums;
 `)
-.then(result => {
-    console.log(results);
-})
-.catch(err)
+  .then(
+    results => {
+      fs.writeFileSync(
+        'albums.json',
+        JSON.stringify(results.rows, true, 2)
+      );
+    },
+    err => console.log(err)
+  )
+  .then (() => {
+    client.end();
+  
+  });
