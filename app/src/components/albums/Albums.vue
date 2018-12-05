a9a398139152ccf1329ff5072bb3a51fb031ff08
‘Michael <pickup.patterns@gmail.com>
2018-12-04 16:48:44 -0800

    adding a place for add an album on albums and added a loading vue

--- app/src/components/albums/Albums.vue
+++ app/src/components/albums/Albums.vue
@@ -1,14 +1,21 @@
 <template>
     <section>
         <h2>Albums</h2>
+        <AddAlbum :onAdd="handleAdd"/>
 
+        <ul v-if="albums">
+            <li v-for="album in albums" :key="album.id">
+                {{album.name}}
+            </li>
+        </ul>
     </section>
 </template>
 
 <script>
+
 export default {
 
-}
+};
 </script>
 
 <style>
