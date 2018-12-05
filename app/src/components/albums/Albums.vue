06f0f5973fbe627855a3ea6bf5cb1c82b03cc48c
‘Michael <pickup.patterns@gmail.com>
2018-12-04 19:47:06 -0800

    rendering form, not rendering list

--- app/src/components/albums/Albums.vue
+++ app/src/components/albums/Albums.vue
@@ -12,10 +12,36 @@
 </template>
 
 <script>
-// import api from '../../services/api';
-// import AddAlbum from './AddAlbum';
-export default {
-
+import api from '../../services/api';
+import AddAlbum from './AddAlbum';
+export default { 
+  data() {
+    return {
+      albums: null,
+      error: null
+    };
+  },
+  components: {
+    AddAlbum
+  },
+  created() {
+    api.getAlbums()
+      .then(albums => {
+        this.albums = albums;
+      })
+      .catch(err => {
+        this.error = err;
+      });
+  },
+  methods: {
+    handleAdd(album) {
+      return api.addAlbum(album)
+        .then(saved => {
+          this.albums.push(saved);
+        });
+    }
+  }
+  
 };
 </script>
 
--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,11 +1,12 @@
 export default {
+
   getAlbums() {
     return fetch('api/albums')
       .then(response => response.json());
   },
 
   addAlbum(album) {
-    return fetch('/api/album', {
+    return fetch('/api/albums', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
