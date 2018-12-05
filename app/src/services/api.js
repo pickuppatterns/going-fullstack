ae17fd10a2506f77d97f30d838f3487a92299fe7
‘Michael <pickup.patterns@gmail.com>
2018-12-04 19:18:45 -0800

    added add Album method to api

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -2,5 +2,16 @@ export default {
   getAlbums() {
     return fetch('api/albums')
       .then(response => response.json());
+  },
+
+  addAlbum(album) {
+    return fetch('/api/album', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(album)
+    })
+      .then(response => response.json());
   }
 };
