2901a4c524cd57095cbdf754986d2e0bd70bdb02
Paige Gorry <paigeegorry@gmail.com>
2018-12-04 18:47:20 -0800

    able to add characters to list - working on shortid

--- server/server.js
+++ server/server.js
@@ -1,1 +1,47 @@
-console.log('server is starting');
+const express = require('express');
+const app = express();
+const shortid = require('shortid');
+const fs = require('fs');
+
+function readData() {
+  const data = fs.readFileSync('./data/characters.json', 'utf8');
+  return JSON.parse(data);
+}
+
+function saveData(characters) {
+  const json = JSON.stringify(characters, true, 2);
+  fs.writeFileSync('./data/characters.json', json);
+}
+
+app.use(express.json());
+
+app.get('/api/characters', (req, res) => {
+  const characters = readData();
+  if(req.query.name) {
+    const match = req.query.name.toLowerCase();
+
+    const filtered = characters.filter(c => {
+      return c.name.toLowerCase().startsWith(match);
+    });
+    res.json(filtered);
+  }
+  else {
+    res.json(characters);
+  }
+});
+
+app.post('/api/characters', (req, res) => {
+  const characters = readData();
+  const character = req.body;
+  characters.push(req.body);
+  character.id = shortid.generate();
+  saveData(characters);
+  res.json(character);
+});
+
+// this should go at the bottom //
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log('server app started on port', PORT);
+});
