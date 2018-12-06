1cee56460d5938cb9dc7de6ef5abc9a83edd4b84
‘Michael <pickup.patterns@gmail.com>
2018-12-05 17:43:46 -0800

    added album detail and wired it to router js

---
+++ app/src/components/albums/AlbumDetail.vue
@@ -0,0 +1,36 @@
+<template>
+    <section v-if="album">
+        <h2>{{album.name}}</h2>
+        <p>
+            {{album.year}}
+        </p>
+        <p>
+            {{album.description}}
+        </p> 
+        <p>
+            {{album.rating}}
+        </p>   
+    </section>
+</template>
+
+<script>
+import api from '../../services/api';
+
+export default {
+  data() {
+    return {
+      student:null
+    };
+  },
+  created() {
+    api.getAlbum(this.$route.params.id)
+      .then(album => {
+        this.album = album;
+      });
+  }
+};
+</script>
+
+<style>
+
+</style>
--- app/src/router.js
+++ app/src/router.js
@@ -1,12 +1,14 @@
 import VueRouter from 'vue-router';
 import Home from './components/home/Home.vue';
 import Albums from './components/albums/Albums.vue';
+import AlbumDetail from './components/albums/AlbumDetail.vue';
 
 export default new VueRouter({
   // mode: 'history',
   routes: [
     { path: '/', component: Home },
     { path: '/albums', component: Albums },
+    { path: '/albums', component: AlbumDetail },
     { path: '*', redirect: '/' }
   ]
 });
