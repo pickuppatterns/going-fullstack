bdd8001bad62a79c0ca8f9b7f180a148a82c173a
‘Michael <pickup.patterns@gmail.com>
2018-12-06 15:11:39 -0800

    swapped logic off create-tables to db-client

--- server/scripts/create-tables.js
+++ server/scripts/create-tables.js
@@ -1,21 +1,14 @@
-const pg = require('pg');
-const Client = pg.Client;
-const databaseUrl = 'postgres://localhost:5432/albums';
+const client = require('../db-client');
 
-const client = new Client(databaseUrl);
-
-client.connect()
-  .then(() => {
-    return client.query(`
-        CREATE TABLE IF NOT EXISTS album (
-            id SERIAL PRIMARY KEY, 
-            name VARCHAR(256) NOT NULL,
-            year INT,
-            description VARCHAR(256),
-            rating INT
-        );
-        `);
-  })
+client.query(`
+  CREATE TABLE IF NOT EXISTS album (
+    id SERIAL PRIMARY KEY, 
+    name VARCHAR(256) NOT NULL,
+    year INT,
+    description VARCHAR(256),
+    rating INT
+    );
+  `)
   .then(
     () => console.log('create tables complete'),
     err => console.log(err)
@@ -23,3 +16,4 @@ client.connect()
   .then(() => {
     client.end();
   });
+ 
