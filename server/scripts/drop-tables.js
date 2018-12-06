bf3139437bfcc3e9350fa918ea972b72810ee508
‘Michael <pickup.patterns@gmail.com>
2018-12-06 15:20:30 -0800

    refactored logic off dropp-table to db-client

--- server/scripts/drop-tables.js
+++ server/scripts/drop-tables.js
@@ -1,15 +1,8 @@
-const pg = require('pg');
-const Client = pg.Client;
-const databaseUrl = 'postgres://localhost:5432/albums';
+const client = require('../db-client');
 
-const client = new Client(databaseUrl);
-
-client.connect()
-  .then(() => {
-    return client.query(`
+client.query(`
     DROP TABLE IF EXISTS album;
-    `);
-  })
+  `)
   .then(
     () => console.log('drop tables complete'),
     err => console.log(err)
